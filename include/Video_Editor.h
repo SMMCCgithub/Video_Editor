b7d8de0f1b8fd1f8328ddb3c9eca8d17c2a98c0f
Sebastian Hyjek <sebby.hyjek@gmail.com>
2024-10-13 13:20:26 -0400

    Add project files.

---
+++ include/Video_Editor.h
@@ -0,0 +1,14 @@
+﻿// Video_Editor.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+
+#include <libavutil/frame.h>
+#include <libavutil/mem.h>
+
+#include <libavcodec/avcodec.h>
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+﻿// Video_Editor.cpp : Defines the entry point for the application.
+//
+
+#include "Video_Editor.h"
+
+int main()
+{
+	printf("Hello CMake.");
+	return 0;
+}
